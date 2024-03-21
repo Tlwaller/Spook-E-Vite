9a9f48deeb6f533e23ec2d8efc85c97aadcd31b0
Trey Waller <53789558+Tlwaller@users.noreply.github.com>
2024-03-21 16:20:44 -0500

    responsive left panel perspective

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,8 +12,12 @@ const Hero = () => {
     <section>
       <div className="box--gradient" />
       <div className="left edge panel">
-        <h2 className="rotate-cw">I AM QUICKLY APPROACHING THE</h2>
-        <h2 className="rotate-ccw">MIDDLE OF MY LIFE 4/27</h2>
+        <div className="left-content">
+          <h2>
+            I AM QUICKLY APPROACHING THE MIDDLE OF MY LIFE. COME TO MY HOUSE TO
+            CELEBRATE ON 4/27.
+          </h2>
+        </div>
       </div>
       <div className="center panel">
         <div className="container">
@@ -22,17 +26,19 @@ const Hero = () => {
         </div>
       </div>
       <div className="right edge panel">
-        <h2>THERE WILL BE</h2>
-        BEER
-        <img alt="beer gif" src={beer} width={"100px"} />
-        PIZZA
-        <img alt="pizza gif" src={pizza} width={"100px"} />
-        SOUL CALIBUR TOUNAMENT
-        <img alt="controller gif" src={controller} width={"100px"} />
-        ROWDY
-        <img alt="cat gif" src={cat} width={"80px"} />
-        PING PONG
-        <img alt="ping-pong gif" src={pingpong} width={"80px"} />
+        <div className="right-content">
+          <h2>THERE WILL BE</h2>
+          BEER
+          <img alt="beer gif" src={beer} width={"100px"} />
+          PIZZA
+          <img alt="pizza gif" src={pizza} width={"100px"} />
+          SOUL CALIBUR TOUNAMENT
+          <img alt="controller gif" src={controller} width={"100px"} />
+          ROWDY
+          <img alt="cat gif" src={cat} width={"80px"} />
+          PING PONG
+          <img alt="ping-pong gif" src={pingpong} width={"80px"} />
+        </div>
       </div>
     </section>
   );
