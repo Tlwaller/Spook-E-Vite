553de67c80a94863a988a8ccf5a658cd28453036
Trey Waller <53789558+Tlwaller@users.noreply.github.com>
2024-03-28 17:17:58 -0500

    populate guest list with db

--- src/components/RSVPForm.js
+++ src/components/RSVPForm.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/RSVPForm.css";
 import btn from "../assets/rsvpBtn.gif";
+import axios from "axios";
 
 const RSVPForm = () => {
+  const [guests, setGuests] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080").then((res) => {
+      setGuests(res.data);
+    });
+  }, []);
+
+  console.log(guests);
   return (
     <div className="rsvp-wrapper">
       <section className="rsvp-container">
@@ -13,7 +22,18 @@ const RSVPForm = () => {
             <img src={btn} />
           </button>
         </form>
-        <div className="rsvp-list-container"></div>
+        <div className="guest-list-container">
+          <ul className="guest-list">
+            {guests.map((guest) => {
+              return (
+                <li className="guest-item" key={guest.id}>
+                  <span className="guest-name">{guest.name}</span>
+                  <span className="guest-message">"{guest.message}"</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </section>
     </div>
   );
